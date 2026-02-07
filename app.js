44fc5ccfbb077982746590e440c637f7a8f3732e
Byron <byron.zapata@uao.edu.co>
2026-02-07 16:41:34 -0500

    Taller en clase

---
+++ app.js
@@ -0,0 +1,22 @@
+//FUNCIÓN REGULAR
+
+function verificarParImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log("El número " + numero + " es par.");
+    } else {
+        console.log("El número " + numero + " es impar.");
+    }
+}
+
+//FUNCIÓN ARROW
+const verificarParImparArrow = (numero) => {
+    if (numero % 2 === 0) {
+        console.log("El número " + numero + " es par.");
+    } else {
+        console.log("El número " + numero + " es impar.");
+    }
+};
+
+//LLAMADAS A LAS FUNCIONES
+verificarParImpar(7);
+verificarParImparArrow(10);
